f6876517a75f06a5cc0aa647bc4ba70487ae544c
radityoarifn <radityo.rt48@gmail.com>
2025-04-10 11:11:43 +0700

    Adding db_config.php

---
+++ backend/config/db_config.php
@@ -0,0 +1,9 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+return [
+    'host' => 'localhost',
+    'db_name' => 'your_database',
+    'username' => 'your_username',
+    'password' => 'your_password'
+];
