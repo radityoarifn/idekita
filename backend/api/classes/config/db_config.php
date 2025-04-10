08157d4adec9614ce9b63c99fa2c5f8a0fc62033
radityoarifn <radityo.rt48@gmail.com>
2025-04-10 11:09:01 +0700

    Delete

--- backend/api/classes/config/db_config.php
+++
@@ -1,9 +0,0 @@
-<?php
-defined('BASEPATH') or exit('No direct script access allowed');
-
-return [
-    'host' => 'localhost',
-    'db_name' => 'your_database',
-    'username' => 'your_username',
-    'password' => 'your_password'
-];
